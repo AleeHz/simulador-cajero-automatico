0aa5da4f0a84be387d6ad9747a5788275a2dc0de
Gonza2002 <ggonzalo592@gmail.com>
2026-06-16 17:11:19 -0300

    Registro usuario V2

--- Registro.py
+++ Registro.py
@@ -1,12 +1,24 @@
 import Login
 
-print("Bienvenido al sistema de registro de usuarios.")
-nombre = input("Ingrese su nombre: ")
-clave = input("Ingrese su clave: ")
-Confirmar_clave = input("Confirme su clave: ")
+def registro():
+    print("Bienvenido al sistema de registro de usuarios.")
+    nombre_n = input("Ingrese su nombre: ")
+    clave_n = input("Ingrese su clave: ")
+    Confirmar_clave = input("Confirme su clave: ")
 
-if clave == Confirmar_clave:
-    print("Registro exitoso. Bienvenido, " + nombre + "!")
-    Login.diccionario_de_usuarios[nombre] = {"contrasenia": clave, "saldo": 0}
-else:
-    print("Las claves no coinciden. Por favor, intente de nuevo.")
+    while clave_n != Confirmar_clave:
+        print("Las claves no coinciden. Por favor, intente de nuevo.")
+        print("\n")
+
+        clave_n = input("Ingrese su clave: ")
+        Confirmar_clave = input("Confirme su clave: ")
+
+    if clave_n == Confirmar_clave:
+        print("Registro exitoso. Bienvenido, " + nombre_n + "!")
+        Login.perfiles_nuevos[nombre_n] = {"contrasenia": clave_n, "saldo": 0, "historial": []}
+        Login.login()
+
+   
+
+
+
